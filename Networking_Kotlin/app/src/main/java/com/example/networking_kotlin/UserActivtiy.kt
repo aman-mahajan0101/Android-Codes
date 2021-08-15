d9a4444970c0e148a3a0efd6fc6cf205e2232888
aman-mahajan0101 <amanmahajan987@gmail.com>
2021-08-15 22:54:48 +0530

    Delete

---
+++ Networking_Kotlin/app/src/main/java/com/example/networking_kotlin/UserActivtiy.kt
@@ -0,0 +1,11 @@
+package com.example.networking_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivtiy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_activtiy)
+    }
+}
---
+++ Networking_Kotlin/app/src/main/java/com/example/networking_kotlin/UserAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.networking_kotlin
+
+class UserAdapter {
+}
